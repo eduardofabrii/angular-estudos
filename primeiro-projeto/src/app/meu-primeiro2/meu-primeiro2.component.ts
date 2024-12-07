fb37ada0e031f6efb698fde443953ca2bdd1f44b
Eduardo Fabri <eduardohfabri@gmail.com>
2024-12-07 17:55:27 -0300

    Inicializando projeto e criando primeiro componente

---
+++ primeiro-projeto/src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PrimeiroComponent } from "../primeiro-projeto/meu-primeiro.component";
+
+@Component({
+  selector: 'app-meu-primeiro2',
+  imports: [PrimeiroComponent],
+  templateUrl: './meu-primeiro2.component.html',
+  styleUrl: './meu-primeiro2.component.css'
+})
+export class MeuPrimeiro2Component {
+
+}
---
+++ primeiro-projeto/src/app/primeiro-projeto/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  standalone: true,
+  imports: [],
+  template: `<h1>Primeiro Component</h1>
+  <p>Criando depois de muita luta!</p>`
+})
+
+export class PrimeiroComponent { }
